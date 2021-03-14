38211151bdc24a05cd8abab103913bfb056642a9
Ryan Anderson <ryan@thalana.co.za>
2021-03-14 09:46:05 +0200

    add get and post requests with Django endpoints

--- telegram_bot/name_setter_bot.py
+++ telegram_bot/name_setter_bot.py
@@ -1,22 +1,83 @@
 from telegram.ext import Updater
 import logging
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, MessageHandler, Filters
+import requests
+from argparse import ArgumentParser
+import os
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+if TELEGRAM_TOKEN is None:
+    logging.info("TELEGRAM_TOKEN not in environment.")
+    TELEGRAM_TOKEN = "1617267363:AAEarpdWi-uRVcTV73Lqy9BPStRoIfwCIvI"
 
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    user = update.message.from_user
+
+    update.message.reply_text(
+        text=f"Hey {user.first_name}! Enter the person you would like to stage for classification."
+    )
 
 
-def main():
+def set_staged_name(update, context):
 
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
-    logger = logging.getLogger(__name__)
+    name = context.args[0]
+
+    logger.info("Setting %s as staged name", name)
+
+    response = post_staged_name_to_django(name)
+
+    if response.ok:
+        update.message.reply_text(
+            text=f"The name {name} has been staged."
+        )
+    else:
+        update.message.reply_text(
+            text="There was an error staging the name.\n"\
+                f"{response.text}"
+        )
+
+
+def get_staged_name(update, context):
+
+    logger.info("Getting staged name")
+    response = get_staged_name_from_django()
+
+    if response.ok:
+        update.message.reply_text(
+            text=f"The current staged name is: {response.json()['name']}."
+        )
+    else:
+        update.message.reply_text(
+            text="There was an error getting the staged name.\n"\
+                f"{response.text}"
+        )
+
+
+def post_staged_name_to_django(name):
+    response = requests.post(set_staged_endpoint, data = {'name': name})
+    logger.info(f"Data returned from post request: {response.text}")
+    return response
+
+
+def get_staged_name_from_django():
+    response = requests.get(get_staged_endpoint)
+    logger.info(f"Data returned from get request: {response.text}")
+    return response
+
+
+def main(dev_or_prod):
 
     # Create the Updater and pass it your bot's token.
-    updater = Updater("1617267363:AAEarpdWi-uRVcTV73Lqy9BPStRoIfwCIvI", use_context=True)
+    updater = Updater(TELEGRAM_TOKEN, use_context=True)
 
-    # if len(sys.argv) == 1:
+    # if dev_or_prod == "prod":
     #     print("deployment")
     #     PORT = int(os.environ.get('PORT', '8443'))
 
@@ -29,21 +90,47 @@ def main():
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    start_handler = CommandHandler('start', start)
-    dp.add_handler(start_handler)
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('stage', set_staged_name, pass_args=True))
+    dp.add_handler(CommandHandler('get_staged', get_staged_name))
     
 
-    # if len(sys.argv) == 2:
+    # if dev_or_prod == "dev":
     print("local dev")
     # Start the Bot
     updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
 if __name__=='__main__':
 
-    main()
+    parser = ArgumentParser(
+        description="Uses telegram bot to post names to django endpoint."
+    )
+
+    parser.add_argument(
+        "--set_staged_endpoint",
+        help="The api endpoint for setting a name as staged.",
+        type=str,
+        default='http://localhost:8000/telegram_hook/set_staged/',
+    )
+    parser.add_argument(
+        "--get_staged_endpoint",
+        help="The api endpoint for getting the staged name.",
+        type=str,
+        default='http://localhost:8000/telegram_hook/get_staged/',
+    )
+    parser.add_argument(
+        "--dev_or_prod",
+        help="Development of production.",
+        type=str,
+        default='dev',
+    )
+
+    args = parser.parse_args()
+
+    set_staged_endpoint = args.set_staged_endpoint
+    get_staged_endpoint = args.get_staged_endpoint
+
+    main(args.dev_or_prod)
